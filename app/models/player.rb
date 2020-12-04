07cffbda6af613ede14a0d615f94b28bdaed9cf4
Drothschild89 <Drothschild89@gmail.com>
2020-12-04 14:27:04 -0800

    Added Batting Averages Method

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,9 +1,10 @@
 class Player < ActiveRecord::Base
-    has_many :contracts
-    has_many :teams, through: :contracts
+        has_many :contracts
+        has_many :teams, through: :contracts
 
-    def self.get_player(first_name, last_name)
-        player = GetRequester.new("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code=%27mlb%27&active_sw=%27Y%27&name_part=%27#{first_name}_#{last_name}%25%27")
+    def self.get_player(name)
+        name_split = name.split(" ")
+        player = GetRequester.new("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code=%27mlb%27&active_sw=%27Y%27&name_part=%27#{name_split[0]}_#{name_split[1]}%25%27")
         player_parsed = player.parse_json
         player_parsed["search_player_all"]["queryResults"]["row"]["player_id"]
     end
@@ -32,6 +33,18 @@ class Player < ActiveRecord::Base
     def self.compare_players(player1, player2)
         self.player_stats(player1)
         self.player_stats(player2)
+    end
+
+    def self.find_batting_average(name)
+        self.add_player(name)
+        if Player.find_by(name: name).position == 'P'
+            puts "Pitcher's do not have a batting average, please enter a player who bats"
+        else
+            player_id = self.get_player(name)
+            player = GetRequester.new("http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id='#{player_id}'")
+            player_parsed = player.parse_json
+            puts player_parsed["sport_career_hitting"]["queryResults"]["row"]["avg"]
+        end
     end
 
 end
